fd1b1d8b8c1a4ce78f1b6e837c7d06629783f3f0
Mariam Pipia <mariampipia8@gmail.com>
2026-06-07 22:05:27 +0400

    'fixes'

---
+++ dist/mini-bank-of-georgia/browser/chunk-ZIYWOVV4.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-YORG5BTH.js";import{n as t,pa as e}from"./chunk-7TIXOJHL.js";var s=()=>{let n=t(r),o=t(e);return n.selected()?!0:(o.navigate(["/bpm/bpm000"]),!1)};export{s as a};
